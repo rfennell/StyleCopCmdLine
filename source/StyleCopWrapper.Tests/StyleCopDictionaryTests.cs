dcba122bdb8150adfe241ffa5f702e76a21779c7
Richard Fennell <richard@blackmarble.co.uk>
2016-06-27 21:40:45 +0100

    Added test for #5. The problem was the dictionaries need to be copied locally, in the same manner as the rule assemblies

---
+++ source/StyleCopWrapper.Tests/StyleCopDictionaryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StyleCopWrapper.Tests
+{
+    [TestClass]
+    public class StyleCopDictionaryTests
+    {
+        [TestMethod]
+        public void Check_a_file_with_spelling_mistake_finds_error()
+        {
+            // arrange
+            var target = new StyleCopWrapper.Wrapper()
+            {
+                SourceFiles = new string[] { @"TestFiles\FileWithSA1650Errors.cs" },
+                SettingsFile = @"TestFiles\SettingsOnlySA1650.StyleCop",
+                AdditionalAddInPaths = new string[] { @"\bin\Debug" }
+            };
+
+            // act
+            target.Scan();
+
+            // assert
+            Assert.AreEqual(false, target.Succeeded);
+            Assert.AreEqual(1, target.ViolationCount);
+        }
+    }
+}
